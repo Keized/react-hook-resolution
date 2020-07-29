f9fff8c14435e9151c2f9e9a1f941ce0413ab013
Kevin <kevin.fiseneck@live.fr>
2020-07-29 22:39:37 +0200

    Updates

---
+++ precache-manifest.efa1be82db8946415de881a0b86a9751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39c003ec2044bc70cc4a5130db38546",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc3ce756225cfa4d935e",
+    "url": "./static/css/main.386de30e.chunk.css"
+  },
+  {
+    "revision": "4005b02da83ce670622f",
+    "url": "./static/js/2.38b768ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.38b768ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc3ce756225cfa4d935e",
+    "url": "./static/js/main.0708ef61.chunk.js"
+  },
+  {
+    "revision": "eac8e5ca4b521d04ca4c",
+    "url": "./static/js/runtime-main.e68ed930.js"
+  }
+]);
