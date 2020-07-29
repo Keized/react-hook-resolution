91859900c324af90296cb5b185f933cb8722396f
Kevin <kevin.fiseneck@live.fr>
2020-07-29 20:06:06 +0200

    /

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # react-hook-resolution
 
-> Get the screen resolution of your app with a simple react hook
+> Get the screen resolution of your page with a simple react hook
 
 [![NPM](https://img.shields.io/npm/v/react-hook-resolution.svg)](https://www.npmjs.com/package/react-hook-resolution) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
--- example/src/App.js
+++ example/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <h1>React hook resolution</h1>
     <h2><a href="https://github.com/Keized/react-hook-resolution">Github</a></h2>
     <div id="device-container">
-      <h3>Your on {resolution.desktop ? 'desktop' : resolution.tablet ? 'tablet (or baby laptop)' : 'mobile'} !</h3>
+      <h3>You're on {resolution.desktop ? 'desktop / laptop' : resolution.tablet ? 'tablet (or baby laptop)' : 'mobile'} !</h3>
       <div id="device" className={resolution.desktop ? 'desktop' : resolution.tablet ? 'tablet' : 'mobile'}>
         <div className="keyboard" />
       </div>
